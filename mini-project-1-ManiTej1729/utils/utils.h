e7ab34742ef4533d678977f173ffcb24bda576de
Mani Tej <149780680+ManiTej1729@users.noreply.github.com>
2024-10-08 00:16:33 +0530

    Update utils.h

--- mini-project-1-ManiTej1729/utils/utils.h
+++ mini-project-1-ManiTej1729/utils/utils.h
@@ -10,5 +10,6 @@ void execute_simple_command(char *command);
 void absolute_to_relative(char *path);
 void tabs_and_spaces(char *command);
 bool is_not_inbuilt(char *command, char *oldwd, char *cwd, int *no_of_bkgd_processes);
+void remove_double_quotes(char *s);
 
 #endif
